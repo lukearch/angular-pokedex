8c55bcb0e9fc843ce58cc6eef531a17910944293
Lucas Larangeira <lukearch.sh@gmail.com>
2022-02-19 01:03:32 -0300

    api test

--- src/app/pages/details/details.component.ts
+++ src/app/pages/details/details.component.ts
@@ -1,15 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { PokeApiService } from 'src/app/service/poke-api.service';
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
-  styleUrls: ['./details.component.scss']
+  styleUrls: ['./details.component.scss'],
 })
 export class DetailsComponent implements OnInit {
+  public getPokemon: any;
+  constructor(private pokeApiService: PokeApiService) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/pages/routing.module.ts
+++ src/app/pages/routing.module.ts
@@ -9,7 +9,7 @@ const routes: Routes = [
     component: HomeComponent,
   },
   {
-    path: 'details/:id',
+    path: 'details/:name',
     component: DetailsComponent,
   },
 ];
